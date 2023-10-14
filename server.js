805035c241770350807afae8f37a88ee2bdcd73a
liethdallal <139842069+liethdallal@users.noreply.github.com>
2023-10-14 14:19:10 -0500

    Got the the server running anf created initial folders

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.listen(port, (req, res) => {
+    console.log(`Running on port ${port} ✅ `)
+})
+
+app.get('/', (req, res) => {
+    res.send("all linked up")
+})

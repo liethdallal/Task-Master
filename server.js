b2e674d9a168ee9e8bae95ce6490709aa5efc1d9
liethdallal <139842069+liethdallal@users.noreply.github.com>
2023-10-15 23:57:56 -0500

    Linked css style to ejs tempelate and rendered it

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ const ejs = require("ejs")
 const app = express()
 const port = 3000
 
-app.listen(port, (req, res) => {
-    console.log(`Running on port ${port} ✅ `)
-})
+app.use(express.static(__dirname + '/views'));
+//We are looking in to the entire folder views to be able to grab the css along with our html
 
 app.set("view engine", "ejs")
 
 app.get('/', (req, res) => {
-    res.render("server.ejs", req.query)
+    res.render('server.ejs')
 })
 
+app.listen(port, (req, res) => {
+    console.log(`Running on port ${port} ✅ `)
+})
 
 

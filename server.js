a7f33033af20434afcbad5c9b81d3ae318fa9c91
liethdallal <139842069+liethdallal@users.noreply.github.com>
2023-10-17 17:11:47 -0500

    Created a route to a sign in page

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require("express")
 const ejs = require("ejs")
 const app = express()
 const port = 3000
+const bodyParser = require("body-parser")
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
 
 app.use(express.static(__dirname + '/views'));
 //We are looking in to the entire folder views to be able to grab the css along with our html
@@ -9,8 +14,11 @@ app.use(express.static(__dirname + '/views'));
 app.set("view engine", "ejs")
 
 app.get('/', (req, res) => {
-    res.render('server.ejs')
+    res.render('homepage')
 })
+app.get('/login', (req, res) => {
+    res.render('signin');
+   });
 
 app.listen(port, (req, res) => {
     console.log(`Running on port ${port} ✅ `)
